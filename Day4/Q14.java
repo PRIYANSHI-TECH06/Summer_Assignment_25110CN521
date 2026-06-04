26fc579f540c50b17cbb11c47074a60570ab6ef7
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-06-04 20:35:41 +0530

    Add files via upload

---
+++ Day4/Q14.java
@@ -0,0 +1,29 @@
+
+    package Day4;
+    import java.util.Scanner;
+    public class Q14{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        int a = 0, b = 1, c = 0;
+
+        if (n == 1)
+            System.out.println(a);
+        else if (n == 2)
+            System.out.println(b);
+        else {
+            for (int i = 3; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            System.out.println("Nth Fibonacci term = " + b);
+        }
+        sc.close();
+    }
+}
+    
+
