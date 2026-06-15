d3ccd66206add147ac56aea0a024a6d63bbeee0a
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-06-15 19:17:04 +0530

    Add files via upload

---
+++ Day8/CharacterTriangle.java
@@ -0,0 +1,12 @@
+package Day8;
+
+public class CharacterTriangle {
+     public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(char ch = 'A'; ch < 'A' + i; ch++) {
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
+}
