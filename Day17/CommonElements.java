5291db50bb80c72b129df48521cefe19c2064801
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-06-18 19:33:35 +0530

    Add files via upload

---
+++ Day17/CommonElements.java
@@ -0,0 +1,33 @@
+package Day17;
+
+import java.util.Scanner;
+
+public class CommonElements {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = sc.nextInt();
+        int[] a = new int[n1];
+
+        for(int i = 0; i < n1; i++)
+            a[i] = sc.nextInt();
+
+        int n2 = sc.nextInt();
+        int[] b = new int[n2];
+
+        for(int i = 0; i < n2; i++)
+            b[i] = sc.nextInt();
+
+        System.out.println("Common Elements:");
+
+        for(int i = 0; i < n1; i++) {
+            for(int j = 0; j < n2; j++) {
+                if(a[i] == b[j]) {
+                    System.out.print(a[i] + " ");
+                    break;
+                }
+                sc.close();
+            }
+        }
+    }
+}
