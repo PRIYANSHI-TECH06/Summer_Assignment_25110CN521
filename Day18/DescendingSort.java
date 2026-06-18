449ad359bf6202c4903d5f8815acee75f2bfad63
PRIYANSHI-TECH06 <priyanshisrivastava173@gmail.com>
2026-06-18 19:34:18 +0530

    Add files via upload

---
+++ Day18/DescendingSort.java
@@ -0,0 +1,30 @@
+package Day18;
+
+import java.util.Scanner;
+
+public class DescendingSort {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i = 0; i < n; i++)
+            arr[i] = sc.nextInt();
+
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Descending Order:");
+        for(int x : arr)
+            System.out.print(x + " ");
+        sc.close();
+    }
+}
